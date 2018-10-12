47259f982d2d8df9ead5fadec0cca0d0d35c6ef1
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2018-10-12 02:28:54 -0400

    changed up css to include input form and start button, started js pseudocode

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-let alphabet = []
+//use input form and submit button to link to next action, 
+//the keys keep score
+//must win 3 out of 5 keys, get 3 keys and winner (win win win song)then go back to start, lose 3 keys and Game Over image, show you lose sprite (they dont want you to win rant then go back to start)
+
+// if input === diddy || p.diddy -- then document.getelebyid and show number with  inner.html in box.    show zoom into key and then go to side to keep score(set interval?) and show you smart gif and play major key then you smart, then you a genius then we da best(win, win win audi) audio and get key (or char move closer), else djk 
+//shaking no keys gif and they dont want you to win audio, they never said winning was easy, some people cant handle success, then congratulations you played urself
+
+//hint plays a song from the artist
+
+//need to link start button to question1.
+//when user inputs answer, if correct go to if/else statement add for loop to i<5 and .length then if else inside that that says 
+//can i make it check for if 3 right go to win, or do i keep it going to end and check there.
+
+
+//looping through array to ask questions?
+
